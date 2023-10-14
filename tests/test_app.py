93db24c7173f830261bc9c1818838a2fafa27e37
Ket Patel <ketz555@hotmail.com>
2023-10-14 12:58:00 +0100

    updated get single album method

--- tests/test_app.py
+++ tests/test_app.py
@@ -16,31 +16,26 @@ def test_get_albums(page, test_web_address, db_connection):
         "Surfer Rosa",
     ])
 
-# def test_get_album_1(page, test_web_address, db_connection):
-#     db_connection.seed("seeds/record_store.sql")
-#     page.goto(f"http://{test_web_address}/albums/1")
-#     h2_tags = page.locator("h2")
-#     paragraph_tags = page.locator("p")
-#     expect(h2_tags).to_have_text([
-#         "Doolittle"
-#     ])
-#     expect(paragraph_tags).to_have_text([
-#         "Released: 1989",
-#         "Artist: Pixies"
-#     ])
+def test_get_album_1(page, test_web_address, db_connection):
+    db_connection.seed("seeds/record_store.sql")
 
-# def test_get_album_2(page, test_web_address, db_connection):
-#     db_connection.seed("seeds/record_store.sql")
-#     page.goto(f"http://{test_web_address}/albums/2")
-#     h2_tags = page.locator("h2")
-#     paragraph_tags = page.locator("p")
-#     expect(h2_tags).to_have_text([
-#         "Surfer Rosa"
-#     ])
-#     expect(paragraph_tags).to_have_text([
-#         "Released: 1988",
-#         "Artist: Pixies"
-#     ])
+    page.goto(f"http://{test_web_address}/albums/1")
+    h1_tags = page.locator("h1")
+    release_year_tags = page.locator(".t-release-year")
+
+    expect(h1_tags).to_have_text("Album: Doolittle")
+    expect(release_year_tags).to_have_text("Released: 1989")
+    
+
+def test_get_album_2(page, test_web_address, db_connection):
+    db_connection.seed("seeds/record_store.sql")
+
+    page.goto(f"http://{test_web_address}/albums/2")
+    h1_tags = page.locator("h1")
+    release_year_tags = page.locator(".t-release-year")
+
+    expect(h1_tags).to_have_text("Album: Surfer Rosa")
+    expect(release_year_tags).to_have_text("Released: 1988")
 
 """
 The page returned by GET /albums should contain a link for each album listed.
@@ -63,6 +58,10 @@ def test_visit_album_show_page_and_go_back(page, test_web_address, db_connection
     page.click("text='Go back to album list'")
     h1_tag = page.locator('h1')
     expect(h1_tag).to_have_text("Albums")
+
+
+
+
 
 
 
